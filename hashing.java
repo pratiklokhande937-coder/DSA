e793796fe670eaab4cc766603aa7ac4a4897c7a3
Pratik Lokhande <pratiklokhande937@gmail.com>
2026-07-15 23:54:34 +0530

    hashing

---
+++ hashing.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class hashing {
+    public static void main(String[]args){
+         Scanner sc = new Scanner(System.in);
+            int n;
+           n = sc.nextInt();
+          int []arr = new int[n];
+          for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+          } 
+          
+          //precompute
+         int [] hash = new int[13];
+         for(int i=0;i<n;i++){
+            hash[arr[i]] +=1;
+         }
+
+
+
+          int q;
+          q=sc.nextInt();
+          while(q > 0){
+            int number;
+            number = sc.nextInt();
+            System.out.println(hash[number]);
+            q--;
+          }
+          sc.close();
+         }
+ 
+
+    
+    }
+
